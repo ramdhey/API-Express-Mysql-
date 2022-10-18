4566874ab3ebf824fa0d907e93950763e0623aa8
Rama Dhea Yudhistira <90076976+ramdhey@users.noreply.github.com>
2022-10-18 21:50:22 +0700

    comit

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,10 @@
 import {Sequelize} from "sequelize";
 
 
-const db = new Sequelize('databasecrud_mysql','root','',{
-    host:'localhost',
-    dialect:'mysql'
+const db = new Sequelize('sql6527516','sql6527516','q2v2yNcJWv',{
+    host:'sql6.freesqldatabase.com',
+    dialect:'mysql',
+    port:'3306'
 });
 
 export default db;
