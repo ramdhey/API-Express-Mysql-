10b7eda5e5e04ffd166a6e7991d7c2f4e29f47c6
Rama Dhea Yudhistira <90076976+ramdhey@users.noreply.github.com>
2022-11-30 23:21:03 +0700

    updt

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # NoteList Link Deploy
 
-http://noteslist.s3-website-ap-northeast-1.amazonaws.com/
-
 ## FrontEnd Link Repository
 
 https://github.com/ramdhey/FrontEnd-API-Express-Mysql
--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import {Sequelize} from "sequelize";
 
 
-const db = new Sequelize('sql6527948','sql6527948','BSU9mT2pv7',{
-    host:'sql6.freesqldatabase.com',
+const db = new Sequelize('ramdheco_sql6527948','ramdheco_sql6527948','BSU9mT2pv7',{
+    host:'server.cloudhebat.com',
     dialect:'mysql',
     port:'3306'
 });
